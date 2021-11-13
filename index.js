1eb4dd8b50fe09d52bd965919f95d0e77c5af21a
NinzeroBk <nicolau.george99@gmail.com>
2021-11-14 00:00:40 +0200

    Added video and vtt

--- index.js
+++ index.js
@@ -10,24 +10,27 @@ app.use("/resources", express.static(__dirname+"/resources"));
 
 app.use(favicon(__dirname+"/favicon.ico"));
 
-app.get("/ceva",function(req,res){    
-    console.log("Am primit o cerere"); 
-    res.write("<p style='color:red'>altceva</p>");
-    res.end();
+app.get(["/","/index","/home"], function(req, res){
+    console.log(req.url, req.ip);
+    res.render("pages/index", {ip:req.ip});
 });
 
-app.get("/", function(req,res){
-    //res.sendFile(__dirname+"/index.html");
-    res.render("pages/index");     //calea sa fie relativa la folderul views
+app.get(["/history"], function(req, res){
+    console.log(req.url, req.ip);
+    res.render("pages/history", {ip:req.ip});
+});
+
+app.get("/*.ejs", function(req, res){
+    res.status(403).render("pages/403")
 });
 
 app.get("/*", function(req, res){
-    console.log(req.url)
+    console.log(req.url, req.ip)
     res.render("pages" + req.url, function(err, resRender){
-        if(err){
-            res.status(404).render("pages/404");
-            return;
-        }
+            if(err.message.includes("Failed to lookup")){
+                res.status(404).render("pages/404");
+                return;
+            }
         res.send(resRender);
     });
 });
